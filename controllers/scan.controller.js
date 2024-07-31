156091044fc69bd3e45c125f1e5cdad867c7cc44
WalidKhalqallah <wkhalqallah2@myges.fr>
2024-07-31 15:19:51 +0200

    scan fix

--- controllers/scan.controller.js
+++ controllers/scan.controller.js
@@ -1,7 +1,6 @@
 const { Scan, Generation } = require("../models");
+const mongoose = require('mongoose');
 const { IPinfoWrapper } = require('node-ipinfo');
-
-const ipinfo = new IPinfoWrapper(process.env.IPINFO_API_TOKEN);
 
 exports.scan = async (req, res) => {
     const { generationId, imageId } = req.params;
@@ -35,3 +34,23 @@ exports.scan = async (req, res) => {
         res.status(500).json("Erreur lors de la création du scan");
     }
 };
+
+exports.getScansByCampaign = async (req, res) => {
+    const { campaignId } = req.params;
+
+    try {
+        const scans = await Scan.find({
+            $and: [
+                { 'generation.campaign': new mongoose.Types.ObjectId(campaignId) },
+                { 'image.campaign': new mongoose.Types.ObjectId(campaignId) },
+            ]
+        }).populate('generation').populate('image');
+        if (!scans) {
+            return res.status(404).json("Scans non trouvés");
+        }
+        res.status(200).json(scans);
+    } catch (error) {
+        console.log("🚀 ~ exports.getScansByCampaign ~ error", error)
+        res.status(500).json("Erreur lors de la récupération des scans")
+    }
+}
--- routes/scan.routes.js
+++ routes/scan.routes.js
@@ -3,5 +3,6 @@ const { requireUser } = require("../middlewares/requireUser.js");
 module.exports = (app) => {
     const scan = require("../controllers/scan.controller.js");
 
+    app.get("/scan/campaign/:campaignId", scan.getScansByCampaign);
     app.get("/scan/:generationId/:imageId", scan.scan);
 }
