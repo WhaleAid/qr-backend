36b173b0237ad7faef832cb861820ba4ec780dbb
WalidKhalqallah <wkhalqallah2@myges.fr>
2024-10-16 17:13:48 +0200

    debug

--- middlewares/deserializeUser.js
+++ middlewares/deserializeUser.js
@@ -4,12 +4,14 @@ exports.deserializeUser = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
     if (!authHeader) {
+        console.log('No authorization header present.');
         return next();
     }
 
     const token = authHeader.split(' ')[1];
 
     if (!token) {
+        console.log('No token found in authorization header.');
         return next();
     }
 
@@ -17,12 +19,16 @@ exports.deserializeUser = (req, res, next) => {
 
     if (payload) {
         req.user = payload;
+        console.log('User deserialized:', req.user);
         return next();
     }
 
     if (expired) {
+        console.log('Token expired.');
         return next();
     }
 
+    console.log('Invalid token.');
     return next();
 };
+
